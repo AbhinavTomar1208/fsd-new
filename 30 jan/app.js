bff6702547ebf11fe0c9852fe7b6887f6aae5c19
AbhinavTomar1208 <abhinavtomar1208@gmail.com>
2026-01-30 12:02:30 +0530

    update 30 jan

--- 30 jan/app.js
+++ 30 jan/app.js
@@ -5,13 +5,16 @@ const root = ReactDOM.createRoot(parent);
 
 root.render(React.createElement("h3", {}, "Loading..."));
 
+// Load first paragraph after 1 second
 setTimeout(() => {
-    const h2 = React.createElement("h2", {}, "Welcome to React dev");
-    const li1 = React.createElement("li", {}, "Java Programming");
-    const li2 = React.createElement("li", {}, "C++ Programming");
+    const para1 = React.createElement("p", {}, "Welcome to React dev - First paragraph loaded!");
+    root.render(para1);
+}, 1000);
 
-    const ul = React.createElement("ul", {}, li1, li2);
-    const div = React.createElement("div", {}, h2, ul);
-
+// Load second paragraph after 3 seconds total (1 + 2)
+setTimeout(() => {
+    const para1 = React.createElement("p", {}, "Welcome to React dev - First paragraph loaded!");
+    const para2 = React.createElement("p", {}, "This is the second paragraph loaded after additional 2 seconds!");
+    const div = React.createElement("div", {}, para1, para2);
     root.render(div);
 }, 3000);
---
+++ 30 jan/tempCodeRunnerFile.js
@@ -0,0 +1,3 @@
+console.log("\n5. Renaming a file...");
+fs.renameSync('myfile.txt', 'newfile.txt');
+co
